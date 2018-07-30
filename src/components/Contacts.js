669f0d3166bf52d023d799f8972bebfd07c79982
Costas-Zeimpekis <morpheustudio@gmail.com>
2018-07-30 13:35:49 +0300

    Adding Event Toggle

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,44 +2,39 @@ import React, { Component } from 'react';
 import Contact from './Contact';
 
 export class Contacts extends Component {
-  constructor() {
-    super();
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: 'John Doe',
+        email: ' jdoe@gmail.com',
+        phone: ' 444-555-555'
+      },
+      {
+        id: 3,
+        name: 'Eleni Doe',
+        email: ' eleni@gmail.com',
+        phone: ' 222-555-555'
+      },
+      {
+        id: 4,
+        name: 'Alex Doe',
+        email: ' alex@gmail.com',
+        phone: ' 333-555-555'
+      }
+    ]
+  };
 
-    this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: 'John Doe',
-          email: ' jdoe@gmail.com',
-          phone: ' 444-555-555'
-        },
-        {
-          id: 3,
-          name: 'Eleni Doe',
-          email: ' eleni@gmail.com',
-          phone: ' 222-555-555'
-        },
-        {
-          id: 4,
-          name: 'Alex Doe',
-          email: ' alex@gmail.com',
-          phone: ' 333-555-555'
-        }
-      ]
-    };
-  }
+  test = 'Costas';
+
   render() {
     const { contacts } = this.state;
     return (
-      <div>
+      <React.Fragment>
         {contacts.map(contact => (
-          <Contact
-            name={contact.name}
-            email={contact.email}
-            phone={contact.phone}
-          />
+          <Contact key={contact.id} contact={contact} />
         ))}
-      </div>
+      </React.Fragment>
     );
   }
 }
