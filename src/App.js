3e2f92d8f6e38e0a8183b59fc3019049af15ea5e
Costas-Zeimpekis <morpheustudio@gmail.com>
2018-07-31 15:22:11 +0300

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { Provider } from './context';
+import Header from './components/layout/Header';
 import Contacts from './components/contacts/Contacts';
 import AddContact from './components/contacts/AddContact';
-import Header from './components/layout/Header';
+import About from './components/Pages/About';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -15,17 +17,21 @@ class App extends Component {
     //   { className: 'App' },
     //   React.createElement('h1', null, 'The App component')
     // );
-    const name = 'Larry';
 
     return (
       <Provider>
-        <div className="App">
-          <Header branding="Contact Manager" />
-          <div className="container">
-            <AddContact />
-            <Contacts />
+        <Router>
+          <div className="App">
+            <Header branding="Contact Manager" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
           </div>
-        </div>
+        </Router>
       </Provider>
     );
   }
--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,67 +1,107 @@
 import React, { Component } from 'react';
+import { Consumer } from '../../context';
+import TextInputGroup from '../layout/TextInputGroup';
+import uuid from 'uuid';
 
 class AddContact extends Component {
   state = {
     name: '',
     email: '',
-    phone: ''
+    phone: '',
+    errors: {}
   };
 
-  onSubmit(e) {
+  onSubmit = (dispatch, e) => {
     e.preventDefault();
-    console.log(this.state);
-  }
+    const { name, email, phone } = this.state;
 
-  onChange(e) {
+    //Check For Errors
+    if (name === '') {
+      this.setState({
+        errors: { name: 'Name is Required' }
+      });
+    }
+
+    if (email === '') {
+      this.setState({
+        errors: { name: 'Name is Required', email: 'Email is Required' }
+      });
+    }
+
+    if (phone === '') {
+      this.setState({
+        errors: {
+          phone: 'Phone is Required'
+        }
+      });
+    }
+
+    const newContact = {
+      id: uuid(),
+      name,
+      email,
+      phone,
+      errors: {}
+    };
+
+    dispatch({
+      type: 'ADD_CONTACT',
+      payload: newContact
+    });
+
+    this.setState({ name: '', email: '', phone: '' });
+  };
+
+  onChange = e => {
     this.setState({ [e.target.name]: e.target.value });
-  }
+  };
 
   render() {
-    const { name, email, phone } = this.state;
+    const { name, email, phone, errors } = this.state;
+
     return (
-      <div className="card mb-3">
-        <div className="card-header">Add Contact</div>
-        <div className="card-body">
-          <form onSubmit={this.onSubmit.bind(this)}>
-            <div className="form-group">
-              <label htmlFor="name">Name</label>
-              <input
-                type="text"
-                name="name"
-                className="form-control form-control-lg"
-                placeholder="Enter Name..."
-                value={name}
-                onChange={this.onChange.bind(this)}
-              />
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card mb-3">
+              <div className="card-header">Add Contact</div>
+              <div className="card-body">
+                <form onSubmit={this.onSubmit.bind(this, dispatch)}>
+                  <TextInputGroup
+                    label="Name"
+                    name="name"
+                    placeholder="Enter Name..."
+                    value={name}
+                    onChange={this.onChange}
+                    error={errors.name}
+                  />
+                  <TextInputGroup
+                    label="Email"
+                    name="email"
+                    placeholder="Enter Email..."
+                    value={email}
+                    type="email"
+                    onChange={this.onChange}
+                    error={errors.email}
+                  />
+                  <TextInputGroup
+                    label="Phone"
+                    name="phone"
+                    placeholder="Enter Phone..."
+                    value={phone}
+                    onChange={this.onChange}
+                    error={errors.phone}
+                  />
+                  <button type="submit" className="btn btn-block btn-light">
+                    Add Contact
+                  </button>
+                </form>
+              </div>
             </div>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                name="email"
-                className="form-control form-control-lg"
-                placeholder="Enter Email..."
-                value={email}
-                onChange={this.onChange.bind(this)}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="name">Phone</label>
-              <input
-                type="text"
-                name="phone"
-                className="form-control form-control-lg"
-                placeholder="Enter Phone..."
-                value={phone}
-                onChange={this.onChange.bind(this)}
-              />
-            </div>
-            <button type="submit" className="btn btn-block btn-light">
-              Add Contact
-            </button>
-          </form>
-        </div>
-      </div>
+          );
+        }}
+      </Consumer>
     );
   }
 }
