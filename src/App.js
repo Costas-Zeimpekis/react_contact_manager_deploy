8fbc6e5ac30bea03d66f554b841ff490c5ba9de2
Costas-Zeimpekis <morpheustudio@gmail.com>
2018-08-01 13:38:45 +0300

    Created Edit Contact

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Provider } from './context';
 import Header from './components/layout/Header';
 import Contacts from './components/contacts/Contacts';
 import AddContact from './components/contacts/AddContact';
+import EditContact from './components/contacts/EditContact';
 import About from './components/Pages/About';
 import NotFound from './components/Pages/NotFound';
 import Test from './components/test/test';
@@ -29,6 +30,7 @@ class App extends Component {
               <Switch>
                 <Route exact path="/" component={Contacts} />
                 <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/test" component={Test} />
                 <Route component={NotFound} />
